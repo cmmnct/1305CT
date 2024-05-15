04a12689991c99f0c47c060db09dda2510540f51
Mark Nijenhuis <mark@cmmnct.nl>
2024-05-15 19:22:16 +0200

    Memory game properties converted to Signals

--- src/app/memory/memory.component.ts
+++ src/app/memory/memory.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal, computed } from '@angular/core';
 import { CardComponent } from '../card/card.component';
 
 @Component({
@@ -10,83 +10,81 @@ import { CardComponent } from '../card/card.component';
 })
 export class MemoryComponent {
 
-cards:Card[] = [ {"name":"dog"},
-{"name":"cat"}, 
-{"name":"rooster"}, 
-{"name":"goose"}, 
-{"name":"chick"}, 
-{"name":"cow"}, 
-{"name":"kitten"}, 
-{"name":"lamb"}, 
-{"name":"mouse"}, 
-{"name":"piglet"}, 
-{"name":"puppy"}, 
-{"name":"duck"}, 
-{"name":"horse"}, 
-{"name":"goat"}, 
-{"name":"sheep"}, 
-{"name":"hen"}, 
-{"name":"pig"}, 
-{"name":"fox"}, 
-{"name":"hedgehog"}, 
-{"name":"peacock"}, 
-{"name":"donkey"}, 
-{"name":"pigeon"}
-]
+  cards = signal<Card[]>([{ "name": "dog" },
+  { "name": "cat" },
+  { "name": "rooster" },
+  { "name": "goose" },
+  { "name": "chick" },
+  { "name": "cow" },
+  { "name": "kitten" },
+  { "name": "lamb" },
+  { "name": "mouse" },
+  { "name": "piglet" },
+  { "name": "puppy" },
+  { "name": "duck" },
+  { "name": "horse" },
+  { "name": "goat" },
+  { "name": "sheep" },
+  { "name": "hen" },
+  { "name": "pig" },
+  { "name": "fox" },
+  { "name": "hedgehog" },
+  { "name": "peacock" },
+  { "name": "donkey" },
+  { "name": "pigeon" }
+  ])
 
-card1:Card = {name:''}
-card2:Card = {name:''}
+  card1 = signal<Card>({ name: '' })
+  card2 = signal<Card>({ name: '' })
 
-deck = this.shuffle(JSON.parse(JSON.stringify(this.cards.concat(this.cards))));
+  deck = computed(() => (this.shuffle(JSON.parse(JSON.stringify(this.cards().concat(this.cards()))))))
 
-  
+  onClickCard(card: Card) {
+    if (!this.card1().name) {
+      this.card1.set(card) 
+      card.exposed = true;
+    } else if (!this.card2().name) {
+      this.card2.set(card)
+      card.exposed = true;
+      setTimeout(() => this.evaluateMatch(), 2000)
+    }
 
-onClickCard(card:Card){
-  if (!this.card1.name){
-    this.card1 = card;
-    card.exposed = true;
-  } else if (!this.card2.name){
-    this.card2 = card;
-    card.exposed = true;
-    setTimeout(() => this.evaluateMatch(),2000)
   }
 
-}
-
-evaluateMatch(){
-if(this.card1?.name === this.card2?.name){
-  this.card1.hidden = true;
-  this.card2.hidden = true; 
-} else {
- this.card1.exposed = false
- this.card2.exposed = false
-}
-this.card1 = {name:''}
-this.card2 = {name:''}
-}
-
-shuffle(array:any) {
-  var m = array.length, t, i;
-
-  // While there remain elements to shuffle…
-  while (m) {
-
-    // Pick a remaining element…
-    i = Math.floor(Math.random() * m--);
-
-    // And swap it with the current element.
-    t = array[m];
-    array[m] = array[i];
-    array[i] = t;
+  evaluateMatch() {
+    if (this.card1().name === this.card2().name) {
+      this.card1().hidden = true;
+      this.card2().hidden = true;
+    } else {
+      this.card1().exposed = false
+      this.card2().exposed = false
+    }
+    this.card1.set({ name: '' })
+    this.card2.set({ name: '' })
   }
 
-  return array;
-}
+  shuffle(array: any) {
+    var m = array.length, t, i;
+
+    // While there remain elements to shuffle…
+    while (m) {
+
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+
+      // And swap it with the current element.
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+
+    return array;
+  }
 }
 export type Card = {
-name:string,
-hidden?:boolean,
-exposed?:boolean,
-id?:number
+  name: string,
+  hidden?: boolean,
+  exposed?: boolean,
+  id?: number
 }
 
